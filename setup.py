5c7f493efdb9cb17d079eca678033b3d5baa5edc
AryanPratapSingh-tech <aryan7800singhpc@gmail.com>
2026-03-30 13:18:55 +0530

    Requirement Installation and MongoDB Setup Done

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Aryan Pratap Singh",
+    author_email= "aryan7800singhpc@gmail.com",
+    packages=find_packages()
+)
